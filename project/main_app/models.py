e70b52b15b80a15c164d4005331b6b4568976f0c
kotelnikov <m89817380346@gmail.com>
2015-10-27 18:33:41 +0300

    create models

--- project/main_app/models.py
+++ project/main_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.contrib.auth.models import User, UserManager
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    """Информация о пользователях"""
+    user = models.OneToOneField(User)
+
+
+
+class SkillCategory(models.Model):
+    """Категории навыков"""
+
+    name = models.CharField(max_length=30,
+                            unique=True,
+                            )
+
+class Skill(models.Model):
+    """Навыки"""
+    name = models.CharField(max_length=128,
+                            unique=True,
+                            )
+    category = models.ForeignKey(SkillCategory)
