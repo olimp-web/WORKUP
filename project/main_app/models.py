ff1647cc797061613e74ae52ff37160c5536fffc
kotelnikov <m89817380346@gmail.com>
2015-10-28 14:28:01 +0300

    change priv choices

--- project/main_app/models.py
+++ project/main_app/models.py
@@ -26,21 +26,24 @@ class Person(models.Model):
     # Пользовательский уровень приватности
     _PRIVATE_SUFFIX = "_MOD_private"
 
+    _PRIVATE_CHOICES = (
+        ('1', 'Видно всем'),
+        ('2', 'Видно друзьям'),
+        ('3', 'Видно колегам'),
+    )
+
     user = models.OneToOneField(User)
     user_name = models.CharField(max_length=100,)
     user_surname = models.CharField(max_length=100,)
     gender = models.CharField(max_length=1,
-                              choices=(
-        ('m', 'Мужской'),
-        ('w', 'Женский')
-    ))
+                              choices=(('m', 'Мужской'),
+                                       ('w', 'Женский'))
+                              )
     birthday = models.DateField()
-    birthday_MOD_private = models.TextField(db_column='birthday_MOD_private',
-                                            choices=(
-        ('1', 'Видно всем'),
-        ('2', 'Видно друзьям'),
-        ('3', 'Видно колегам')
-    ))
+    birthday_MOD_private = models.CharField(max_length=1,
+                                            db_column='birthday_MOD_private',
+                                            choices=_PRIVATE_CHOICES
+                                            )
 
     # def get_pivate(self, attr):
     #     suffix = self._PRIVATE_SUFFIX
@@ -56,3 +59,5 @@ class UserSkills(models.Model):
     person = models.ForeignKey(Person)
     skill = models.ForeignKey(Skill)
     grade = models.SmallIntegerField(help_text='Уровнь владения навыком')
+    private = models.CharField(max_length=1,
+                               choices=Person._PRIVATE_CHOICES)
