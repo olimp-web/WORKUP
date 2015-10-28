94f6be82acb15ea10acf0d786898ceb637bfc54b
kotelnikov <m89817380346@gmail.com>
2015-10-28 05:47:06 +0300

    Create Peson-Skill link. Add field user_name, user_surname, gender and birthday to Person

---
+++ project/main_app/migrations/0001_initial.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Skill',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=128, unique=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='SkillCategory',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=30, unique=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='UserProfile',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='skill',
+            name='category',
+            field=models.ForeignKey(to='main_app.SkillCategory'),
+        ),
+    ]
--- project/main_app/models.py
+++ project/main_app/models.py
@@ -1,12 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User, UserManager
-# Create your models here.
-
-
-class UserProfile(models.Model):
-    """Информация о пользователях"""
-    user = models.OneToOneField(User)
-
 
 
 class SkillCategory(models.Model):
@@ -16,9 +9,50 @@ class SkillCategory(models.Model):
                             unique=True,
                             )
 
+
 class Skill(models.Model):
     """Навыки"""
     name = models.CharField(max_length=128,
                             unique=True,
                             )
     category = models.ForeignKey(SkillCategory)
+
+
+class Person(models.Model):
+    """
+        Информация о пользователях
+    """
+
+    #Пользовательский уровень приватности
+    _PRIVATE_SUFFIX = "__private"
+
+    user = models.OneToOneField(User)
+    user_name = models.CharField(max_length=100,)
+    user_surname = models.CharField(max_length=100,)
+    gender = models.CharField(max_length=1,
+                              choices=(
+        ('m', 'Мужской'),
+        ('w', 'Женский')
+    ))
+    birthday = models.DateField()
+    birthday_MOD_private = models.TextField(db_column='birthday_MOD_private',
+                                            choices=(
+        ('1', 'Видно всем'),
+        ('2', 'Видно друзьям'),
+        ('3', 'Видно колегам')
+    ))
+
+    # def get_pivate(self, attr):
+    #     suffix = self._PRIVATE_SUFFIX
+    #     # if hasattr(self, (attr.__name__+ suffix)):
+    #     return getattr(self, attr.__name__)
+
+#
+#
+    skills = models.ManyToManyField(Skill, through='UserSkills')
+
+
+class UserSkills(models.Model):
+    person = models.ForeignKey(Person)
+    skill = models.ForeignKey(Skill)
+    grade = models.SmallIntegerField(help_text='Уровнь владения навыком')
