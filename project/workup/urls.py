08dc79d380509aba6f3c51e1542154c41570b958
kotelnikov <m89817380346@gmail.com>
2015-11-03 18:51:38 +0300

    begin creating urls

--- project/workup/urls.py
+++ project/workup/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^/adm/', include(admin.site.urls)),
+    url(r'^/', include('main_app.view', namespace='main')),
 ]
