0fc0812aaed71045eff368f408761fc400f65ebd
kotelnikov <m89817380346@gmail.com>
2015-11-18 04:32:24 +0300

    Create admin view Person

--- project/main_app/admin.py
+++ project/main_app/admin.py
@@ -2,10 +2,28 @@ from django.contrib import admin
 from .models import Person, Skill, UserSkills, SkillCategory, Contact, City, Education
 # Register your models here.
 
-admin.site.register(Person)
+# admin.site.register(Person)
 admin.site.register(Skill)
 admin.site.register(SkillCategory)
-admin.site.register(Contact)
+# admin.site.register(Contact)
 admin.site.register(City)
-admin.site.register(Education)
+# admin.site.register(Education)
 
+
+class EducationInLine(admin.StackedInline):
+    model = Education
+    extra = 1
+
+
+class ContactInLine(admin.TabularInline):
+    model = Contact
+    extra = 2
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    inlines = [
+        ContactInLine,
+        EducationInLine,
+
+    ]
