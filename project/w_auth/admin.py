fc2d6f0cac08d4b5df441a77c4c670b8fb1a2b37
leenr <i@leenr.ru>
2015-11-19 22:24:57 +0300

    Functionalize w_auth admin site

--- project/w_auth/admin.py
+++ project/w_auth/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
 from .models import Account
 
 
-@admin.register(Account)
-class AccountAdmin(admin.ModelAdmin):
+class AccountAdmin(UserAdmin):
     list_display = ('email', 'is_active', 'is_staff', 'is_superuser', 'created', 'last_login')
     fieldsets = (
         (None, {
@@ -19,8 +20,14 @@ class AccountAdmin(admin.ModelAdmin):
     )
     list_filter = ('is_active', 'is_staff', 'is_superuser')
     ordering = ('created',)
-    readonly_fields = ('created', 'last_login', 'password')
+    readonly_fields = ('created', 'last_login')
     search_fields = ('email', 'created')
-#     TODO add 'set password' btn.
-#     TODO change create form.
-#     Look import django.contrib.auth.admin
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2')}
+        ),
+    )
+
+admin.site.register(Account, AccountAdmin)
